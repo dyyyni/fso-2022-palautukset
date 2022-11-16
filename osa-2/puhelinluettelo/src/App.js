97f52a37e0905fe557a590a9736607bd026f0171
Daniel Luoma <daniel.luoma@vincit.fi>
2022-11-16 18:04:31 +0200

    puhelinluettelo step 3 done

--- osa-2/puhelinluettelo/src/App.js
+++ osa-2/puhelinluettelo/src/App.js
@@ -2,26 +2,46 @@ import { useState } from 'react';
 import Person from './components/Person';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
 
-    const alreadyInPhonebook = persons.find(
+    const nameIsInPhonebook = persons.find(
       (person) => person.name === newName
     )
       ? true
       : false;
 
-    if (alreadyInPhonebook) {
+    if (nameIsInPhonebook) {
       window.alert(`${newName} is already added in phonebook.`);
+    } else {
+      addContent();
+    }
+  };
+
+  const addContent = () => {
+    const contentInNameField = newName !== '' ? true : false;
+    const contentInNumberField = newNumber !== '' ? true : false;
+
+    if (!contentInNameField)
+      window.alert(`No name is specified for the number.`);
+    else if (!contentInNumberField) {
+      window.alert(`No number is specified for the name.`);
     } else {
       const personObject = {
         name: newName,
+        number: newNumber,
       };
-
       setPersons(persons.concat(personObject));
+      setNewNumber('');
       setNewName('');
     }
   };
@@ -31,12 +51,21 @@ const App = () => {
     setNewName(event.target.value);
   };
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number:{' '}
+          <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
